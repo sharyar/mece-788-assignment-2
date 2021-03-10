9e161395d713e46fbdf75bb37df278b78c833f82
Sharyar Memon <sharyar@ualberta.ca>
2021-03-10 12:49:07 -0700

    feat: completes Mutation algorithm for binary feature selection

--- evolutionary_feature_selection/Mutation_Incomplete.m
+++ evolutionary_feature_selection/Mutation_Incomplete.m
@@ -2,16 +2,19 @@ function [Offspring] = Mutation_Incomplete(Parent, mu)
 % This Function Perform Bianry Mutation by Changing Some Genes from 0 to 1
 % or Vice Versa.
 
-Offspring = [];
-
 % *********** WRITE YOUR CODE HERE *************
 % How Many Genes Must Undergo Mutation (Calculate Using mu)
 % NumGenes Must be between 1 and length(Parent)
 
 % Select NumGenes of Parent Randomly
+num_genes_to_mutate = round(mu * length(Parent));
+Offspring = Parent;
 
 % Apply Mutation
-
+for i=1:num_genes_to_mutate
+    % will invert a 1 to 0 and 0 to 1!
+    Offspring(i) = abs(Offspring(i) - 1);
+end
 
 % **********************************************
 
